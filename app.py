c9a87da033f0ced18b7dd92850e1e2f2e467c39b
Omar Elsendiony <omar.sendiony@gmail.com>
2025-09-14 01:09:12 +0300

    remove the unused var

--- app.py
+++ app.py
@@ -127,15 +127,9 @@ GOOGLE_DISCOVERY_URL = (
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Naive database setup
-# try:
-#     init_db_command()
-# except sqlite3.OperationalError:
-#     # Assume it's already been created
-#     pass
 
 # OAuth 2 client setup
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
+# client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 # Flask-Login helper to retrieve a user from our db
 @login_manager.user_loader
