e733f86533249ac40f7d04884419757c21746945
Omar Elsendiony <omar.sendiony@gmail.com>
2025-09-19 15:25:23 +0300

    change the route

--- modules/interface_connections.py
+++ modules/interface_connections.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 interface_connections_bp = Blueprint('interface_connections', __name__)
 
-@interface_connections_bp.route('/', strict_slashes=False, methods=["GET", "POST"])
+@interface_connections_bp.route('/interface_connections', strict_slashes=False, methods=["GET", "POST"])
 def interface_connections():
     return render_template('interface_connections.html')
