d8ad07f8972f5a6caef4fb4797e27c2d1e598ade
Omar Elsendiony <omar.sendiony@gmail.com>
2025-10-09 14:57:54 +0300

    removing print statement

--- modules/task_framework.py
+++ modules/task_framework.py
@@ -370,7 +370,7 @@ def execute_api():
         }), 405
 
     passed_data = request.get_json()
-    print(passed_data)
+    # print(passed_data)
     # print(passed_data.get('environment'))
     environment = passed_data.get('environment', session.get("environment"))
     ENVS_PATH = "envs"
